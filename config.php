c3f38c583a5d4d1ac777cd3d7a02b144ad960760
Kelly Eames <46878406+kellyeames@users.noreply.github.com>
2019-11-26 17:45:51 -0800

    Add files via upload

--- config.php
+++ config.php
@@ -1,57 +1,57 @@
 <?php
-
+ 
 ob_start(); //prevents header errors
 define('DEBUG',TRUE); //see all errors
-//include('credentials.php');
-
+include('credentials.php');
+ 
 $siteName = 'The Page for all Things Doggo!';
 //$tagLine - 'Headline 2';
-
+ 
 define('THIS_PAGE', basename($_SERVER['PHP_SELF'])); 
-
+ 
 $nav['index.php'] = 'Home';
 $nav['about.php'] = 'About';
 $nav['daily.php'] = 'Daily';
 $nav['breeds.php'] = 'Breeds';
 $nav['contact.php'] = 'Contact';
 $nav['images.php'] = 'Gallery';
-
+ 
 switch(THIS_PAGE) {
 case 'index.php':
 $title = 'Home page of doggo site';
 $bodyClass = 'home';
 break;
-
+ 
 case 'about.php':
 $title = 'About page of doggo site';
 $bodyClass = 'about inner';
 break;
-
+ 
 case 'daily.php':
 $title = 'Daily page of doggo site';
 $bodyClass = 'daily inner';
 break;
-
+ 
 case 'breeds.php':
 $title = 'Breed description page of doggo site';
 $bodyClass = 'breeds';
 break;
-
+ 
 case 'contact.php':
 $title = 'Contact page of doggo site';
 $bodyClass = 'contact inner';
 break;
-
+ 
 case 'images.php':
 $title = 'My table of images';
 $bodyClass = 'images inner';
 break;
-
+ 
 default:
 $title = THIS_PAGE;
 }
-
-
+ 
+ 
 function myerror($myFile, $myLine, $errorMsg)
 {
     if(defined('DEBUG') && DEBUG)
@@ -61,12 +61,11 @@ function myerror($myFile, $myLine, $errorMsg)
         echo "Error Message: <b>" . $errorMsg . "</b><br />";
         die();
     }else{
-        echo "I'm sorry, we have encountered an error. Would
-        you like to buy a dog?";
+        echo "I'm sorry, we have encountered an error.";
         die();
     }
 }
-
+ 
 function makeLinks($nav) {
 $myReturn = '';
 foreach(nav as $key => $value) {
@@ -84,14 +83,14 @@ foreach(nav as $key => $value) {
     
     return $myReturn;
 } //end function
-
-
+ 
+ 
 //form php!!!
 $name = $email = $gender = $comments = $dogs = $phone = $privacy = '';
 $nameErr = $emailErr = $genderErr = $commentsErr = $dogsErr = $phoneErr = $privacyErr = '';
-
-
-
+ 
+ 
+ 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
     
 if(empty($_POST['name']) ) {
@@ -99,37 +98,37 @@ $nameErr = 'Please fill out your name';
 } else {
 $name = $_POST['name'];
 }
-
+ 
 if(empty($_POST['email']) ) {
 $emailErr = 'Please fill out your email';
 } else {
 $email = $_POST['email'];
 }
-
+ 
 if(empty($_POST['gender']) ) {
     $genderErr = 'Please choose your gender';
     } else {
     $gender = $_POST['gender'];
     }
-
+ 
     if(empty($_POST['privacy']) ) {
         $privacyErr = 'Please check privacy';
         } else {
         $privacy = $_POST['privacy'];
         }
-
+ 
     if(empty($_POST['dogs']) ) {
         $dogsErr = 'Please choose your dogs';
         } else {
         $dogs = $_POST['dogs'];
         }
-
+ 
         if(empty($_POST['comments']) ) {
             $commentsErr = 'Please add your comments';
             } else {
             $comments = $_POST['comments'];
             }
-
+ 
         if(empty($_POST['phone'])) {  // if empty, type in your number
             $telErr = 'Your phone number please!';
             } elseif(array_key_exists('tel', $_POST)){
@@ -140,28 +139,28 @@ if(empty($_POST['gender']) ) {
             $phone = $_POST['phone'];
             }
             }
-
-
-
-
-
+ 
+ 
+ 
+ 
+ 
 if(isset($_POST['name'],
 $_POST['email'],
 $_POST['gender'],
 $_POST['privacy'],
 $_POST['comments'],
 $_POST['dogs'])) {
-
-
+ 
+ 
 function myDogs() {
 $myReturn = '';
 if(!empty($_POST['dogs'])) {
 $myReturn = implode(",",$_POST['dogs']);
-
+ 
 } return $myReturn; // end if not empty
-
+ 
 } // end of function
-
+ 
 $to = 'k.leanne.e@gmail.com';
 $subject = 'Test Email' .date("m/d/y");
 $body = '
@@ -175,8 +174,8 @@ $body = '
 $headers = array(
     'From' => 'no-reply@gmail.com',
     'Reply-to' => ''.$email.'');
-
-
+ 
+ 
 if($_POST['name']!="" && $_POST['email']!="" && $_POST ['dogs']!="" &&
 $_POST['comments']!="" && $_POST['gender']!="" && $_POST['phone']!=""
 && $_POST['privacy']!="")
@@ -184,15 +183,15 @@ $_POST['comments']!="" && $_POST['gender']!="" && $_POST['phone']!=""
     mail($to, $subject, $body, $headers);
     header("Location: thx.php");
 }
-
-
+ 
+ 
 } // end isset
-
-
+ 
+ 
 } // end server request
-
+ 
 //MY GALLERY
-
+ 
 $dog['Lassie'] = 'lassi_Lassie Come Home';
 $dog['Toto'] = 'totod_Wizard of Oz';
 $dog['Rin_Tin_Tin'] = 'rinti_Where The North Begins';
@@ -204,10 +203,10 @@ $dog['Buddy'] = 'buddy_Air Bud';
 $dog['Comet'] = 'comet_Full House';
 $dog['Hooch'] = 'hooch_Turner and Hooch';
 $dog['Marley'] = 'marle_Marley and Me';
-
-
+ 
+ 
 //daily page php
-
+ 
 if(isset($_GET['today'])){  //if it is set, do the following
     $today = $_GET['today'];
     }else {         //if not, do the following
@@ -266,7 +265,9 @@ if(isset($_GET['today'])){  //if it is set, do the following
     break;
     
     }
-
+ 
     //database php
-
+ 
   
+ 
+
